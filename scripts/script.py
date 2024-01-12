37c7906e8066ccee57a9521da9289ac94f12079c
Jan2134 <jan.stb8@gmail.com>
2024-01-13 00:05:35 +0100

    Update script.py

---
+++ scripts/script.py
@@ -0,0 +1,30 @@
+"""
+Filter through Movie Dataset
+"""
+
+import pandas as ps
+import click
+
+class FilterMovies:
+    """
+    Class to filter the movie ratings
+    """
+    def __init__(self, ):
+        
+    def filter_by_
+
+    def filter_by_
+
+
+@click.command()
+@click.option()
+@click.option()
+
+def main():
+    """
+    
+    """
+
+
+if __name__ == "__main__":
+    main()
